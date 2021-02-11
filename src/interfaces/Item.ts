a86b3c546c61b006d54c9924325744bcc25dfa00
Alexandre Henrique <zaleortgames@outlook.com>
2021-02-11 22:11:40 +0100

    API Objects

---
+++ src/interfaces/Item.ts
@@ -0,0 +1,17 @@
+export default interface Item {
+  id: number;
+  deleted: boolean;
+  type: 'job' | 'story' | 'comment' | 'poll' | 'pollopt';
+  by: string;
+  time: number;
+  text: string;
+  dead: boolean;
+  parent: number;
+  poll: object;
+  kids: number[];
+  url: string;
+  score: string;
+  title: string;
+  parts: object;
+  descendants: number;
+}
---
+++ src/interfaces/User.ts
@@ -0,0 +1,7 @@
+export default interface User {
+  id: number;
+  created: number;
+  karma: number;
+  about: string;
+  submited: number[]; 
+}
