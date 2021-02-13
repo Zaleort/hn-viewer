44ab633b39ff94458a41c6f2ce2cfef0706cfca3
Alexandre Henrique <zaleortgames@outlook.com>
2021-02-13 02:48:44 +0100

    Mostrar top 20 stories en Home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,64 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+  <main class="hn-section">
+    <story
+      v-for="(story, index) of stories"
+      :key="story.id"
+      v-bind="story"
+      :index="index"
+    />
+  </main>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import { defineComponent, onMounted, ref } from 'vue';
+import Item from '@/interfaces/Item';
+import Story from '@/components/Story.vue';
+import api from '@/lib/ApiStories';
 
 export default defineComponent({
   name: 'Home',
   components: {
-    HelloWorld,
+    Story,
+  },
+
+  setup: () => {
+    const stories = ref<Item[]>([]);
+    const storiesIds = ref<number[]>([]);
+    const count = ref(0);
+
+    const getStories = async () => {
+      if (!storiesIds.value || storiesIds.value.length === 0) return;
+
+      const promises = [];
+
+      for (let i = count.value; i < count.value + 20; i++) {
+        promises.push(api.getOne(storiesIds.value[i]));
+      }
+
+      count.value += 20;
+      const values: Item[] = await Promise.all(promises);
+
+      values.forEach(story => {
+        const index = storiesIds.value.indexOf(story.id);
+        stories.value[index] = story;
+      });
+    };
+
+    const getIds = async () => {
+      storiesIds.value = await api.getTop();
+      getStories();
+    };
+
+    onMounted(() => {
+      getIds();
+    });
+
+    return {
+      stories,
+      count,
+      getIds,
+      getStories,
+    };
   },
 });
 </script>
