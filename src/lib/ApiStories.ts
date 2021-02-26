d948a034b9075c45b41d1aadaeb449c830598046
Alexandre Henrique <zaleortgames@outlook.com>
2021-02-26 17:52:43 +0100

    Especificar qué datos devuelve las llamadas a API

--- src/lib/ApiStories.ts
+++ src/lib/ApiStories.ts
@@ -1,14 +1,16 @@
+import Item from '@/interfaces/Item';
+import User from '@/interfaces/User';
 import api from '@/lib/ApiService';
 
 const url = 'https://hacker-news.firebaseio.com/v0/';
 
 export default {
-  getNew: () => api.get(`${url}newstories.json`),
-  getTop: () => api.get(`${url}topstories.json`),
-  getBest: () => api.get(`${url}beststories.json`),
-  getAsk: () => api.get(`${url}askstories.json`),
-  getShow: () => api.get(`${url}showstories.json`),
-  getJob: () => api.get(`${url}jobstories.json`),
-  getOne: (id: number) => api.get(`${url}item/${id}.json`),
-  getUser: (id: number) => api.get(`${url}user/${id}.json`),
+  getNew: (): Promise<number[]> => api.get(`${url}newstories.json`),
+  getTop: (): Promise<number[]> => api.get(`${url}topstories.json`),
+  getBest: (): Promise<number[]> => api.get(`${url}beststories.json`),
+  getAsk: (): Promise<number[]> => api.get(`${url}askstories.json`),
+  getShow: (): Promise<number[]> => api.get(`${url}showstories.json`),
+  getJob: (): Promise<number[]> => api.get(`${url}jobstories.json`),
+  getOne: (id: number): Promise<Item> => api.get(`${url}item/${id}.json`),
+  getUser: (id: number): Promise<User> => api.get(`${url}user/${id}.json`),
 };
